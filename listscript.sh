18ecf50a2d7dfe3d4ee92b71f409487b9f474394
Luke Hackworth <luketheduke0@users.noreply.github.com>
2015-07-30 19:10:10 -0500

    added files

---
+++ listscript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#base for automating the execution of a script with a single paramater
+#multiple times.
+#Based from http://tldp.org/LDP/abs/html/othertypesv.html
+
+MINPARAM=1
+LISTLOCATION="" #Place file location here, including file extension
+
+#[1] Improper # of parameter handling and help
+if [ $# -lt MINPARAM ]
+then
+ echo "There are too few paramaters. try --help"
+fi
+if [ $1 = "--help" ]
+then
+ echo "This script runs a separate command for each line in the list at the"
+ echo "list given at LISTLOCATION."
+fi
+#[END 1]
+
+#[2] run command for each line in LISTLOCATION
+if [ $1 = "--run" ]
+then
+ cat $LISTLOCATION | while read line
+ do
+  echo "" #Put command where echo is, eg. ./script --run $line
+ done
+fi
+#[END 2]
