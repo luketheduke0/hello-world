092fb2242a96d1ed349ec8d0fe8e726ddad948b2
Luke Hackworth <luketheduke0@users.noreply.github.com>
2015-08-01 01:25:44 -0500

    renamed files and added ipportscan

---
+++ 3-ipportscan.sh
@@ -0,0 +1,15 @@
+a=0
+LISTLOCATION="iplist.txt"
+MINPARAM=1
+if [ $# -lt $MINPARAM ]
+then
+ echo "Enter a number, it'll nmap it"
+fi
+cat $LISTLOCATION | while read line
+do
+ if [ $1 = $a ]
+ then
+  nmap $line
+ fi
+ ((a+=1))
+done
