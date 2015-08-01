8becb1d62d473f8dc33acd9147d2893349292d9a
Luke Hackworth <luketheduke0@users.noreply.github.com>
2015-08-01 01:12:31 -0500

    Make file location a variable.

--- ipcollect.sh
+++ ipcollect.sh
@@ -1,7 +1,8 @@
 a=1
+LISTLOCATION=iplist.txt
 
 while [ $a=1 ]
 do
- nmap -iR 150 -n -p 80,443 --open | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> iplist.txt
+ nmap -iR 150 -n -p 80,443 --open | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> $LISTLOCATION
 done
 
