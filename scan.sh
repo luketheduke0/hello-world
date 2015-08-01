a30be7f56d36aa29488dc3987fec1a4b617918ba
Luke Hackworth <luketheduke0@users.noreply.github.com>
2015-07-31 23:45:18 -0500

    Add some stuff. Still isn't working quite right.

--- scan.sh
+++ scan.sh
@@ -4,11 +4,14 @@ a=1
 
 while [ $a=1 ]
 do
- nmap -iR 500 -n -p 80 --open | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > iplist.txt
+ nmap -iR 5 -n -p 80 --open | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> iplist.txt
  cat iplist.txt | while read line
  do
   firefox $line
  done
- $a++
+ if [ $1 = C ]
+ then
+  a++
+ fi
 done
 
