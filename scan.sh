771521d70fa2690f7db1f6216987134c057b2b37
Luke Hackworth <luketheduke0@users.noreply.github.com>
2015-07-31 23:53:33 -0500

    Almost functioning now.
    Had to remove the #/bin/sh line. Thinking about splitting it
    into two scripts that can be run separately. One script to
    collect IP's, another to browse them.

--- scan.sh
+++ scan.sh
@@ -1,17 +1,11 @@
-$!/bin/sh
-
 a=1
 
 while [ $a=1 ]
 do
- nmap -iR 5 -n -p 80 --open | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> iplist.txt
+ nmap -iR 5 -n -p 80 --open | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > iplist.txt
  cat iplist.txt | while read line
  do
   firefox $line
  done
- if [ $1 = C ]
- then
-  a++
- fi
 done
 
