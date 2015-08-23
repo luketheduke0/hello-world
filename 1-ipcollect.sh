c2c055eae25b60ab80d0723b6fd0dfa7110bcffa
Luke H <luketheduke0@gmail.com>
2015-08-22 23:40:46 -0500

    Add more common http ports. Slightly slower.

--- 1-ipcollect.sh
+++ 1-ipcollect.sh
@@ -1,8 +1,28 @@
 a=1
 LISTLOCATION=iplist.txt
 
+httpList=common-http-ports.txt
+ipArray=()
+int=0
+ipString=""
+
+#reads from ip list, forms array from it, then creates int list formatted for nmap
+while read -r line
+do
+    testArray[$int]=$line
+    #echo $line
+    if [ $int -ne 0 ]; then
+          ipString+=","$line
+     else
+          ipString+=$line
+     fi
+     let "int+=1"
+done < "$httpList"
+
+#echo $ipString
+
+#nmap loop
 while [ $a=1 ]
 do
- nmap -iR 150 -n -p 80,443 --open | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> $LISTLOCATION
+ nmap -iR 150 -n -p $ipString --open | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> $LISTLOCATION
 done
-
