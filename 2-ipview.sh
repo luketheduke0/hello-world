d07718589333360e5df8fa637e4339ebaac29320
Luke H <luketheduke0@gmail.com>
2015-08-29 23:34:20 -0500

    Add option for viewing US IP cleaned IPs

--- 2-ipview.sh
+++ 2-ipview.sh
@@ -6,6 +6,12 @@ if [ $# < $MINPARAM ]
 then
   echo "Enter a number, it'll show it in firefox"
 fi
+
+if [ $2 = clean ]
+then
+LISTLOCATION=usa_removed_iplist.txt
+fi
+
 cat $LISTLOCATION | while read line
 do
   if [ $1 = $a ]
