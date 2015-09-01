bd1ce186d31cef59a8be37da24e06fcf27e3a3e9
Luke H <luketheduke0@gmail.com>
2015-09-01 00:44:35 -0500

    So much productive insomnia

--- americanipremoverewrite.py
+++ americanipremoverewrite.py
@@ -1,8 +1,7 @@
 
 
 import csv
-import sys
-print(sys.version)
+import ipaddress
 
 infile_path = 'iplist.txt'
 ip_ranges_file_path = 'us_ips.csv'
@@ -12,9 +11,15 @@ ip_ranges = []
 with open(ip_ranges_file_path, 'r', newline='') as ip_ranges_file:
     spamreader = csv.reader(ip_ranges_file)
     for row in spamreader:
-        ip_range_min = str(row[0])
-        ip_range_max = str(row[1])
-        ip_ranges.append((ip_range_min, ip_range_max))
-
+        if(row != ""):
+            ip_range_min = str(row[0])
+            ip_range_max = str(row[1])
+            ip_ranges.append((ip_range_min, ip_range_max))
+    
+with open(infile_path, 'r') as ip_strs, open(outfile_path, 'a') as outfile:
+    for ip_str in ip_strs:
+        print(ip_str[0])
+        ip = ipaddress.ip_address(ip_str[0])
+        print(str(ip))
 
     
